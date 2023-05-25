7c12d03c2c340e779b6c42d602ec66f390d3a108
Peter Rasmussen <peter@rustos.org>
2023-05-25 09:57:46 +0200

    simplify server

--- server.py
+++ server.py
@@ -1,42 +1,17 @@
 import http.server
 import socketserver
-from os import path
-
-my_host_name = 'localhost'
-my_port = 8888
-my_html_folder_path = './'
-
-index_path = 'index.html'
-
 
 class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
-
-    def _set_headers(self):
-        self.send_response(200)
-        self.send_header('Content-Type', 'text/html')
-        self.send_header('Content-Length', path.getsize(self.getPath()))
-        self.end_headers()
-
-    def getPath(self):
-        if self.path == '/':
-            content_path = path.join(
-                my_html_folder_path, index_path)
-        else:
-            content_path = path.join(my_html_folder_path, str(self.path).split('?')[0][1:])
-        return content_path
-
-    def getContent(self, content_path):
-        with open(content_path, mode='r', encoding='utf-8') as f:
-            content = f.read()
-        return bytes(content, 'utf-8')
-
     def do_GET(self):
-        self._set_headers()
-        self.wfile.write(self.getContent(self.getPath()))
+        if self.path == '/':
+            self.path = 'index.html'
+        return http.server.SimpleHTTPRequestHandler.do_GET(self)
 
+# Create an object of the above class
+handler_object = MyHttpRequestHandler
 
-my_handler = MyHttpRequestHandler
+PORT = 8000
+server = socketserver.TCPServer(("", PORT), handler_object)
 
-with socketserver.TCPServer(("", my_port), my_handler) as httpd:
-    print("Http Server Serving at port", my_port)
-    httpd.serve_forever()
+# Star the server
+server.serve_forever()
