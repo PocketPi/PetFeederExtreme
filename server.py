203d3fee3583b8509d5d277795f25d6a03dfc207
Peter Rasmussen <peter@rustos.org>
2023-05-25 22:21:45 +0200

    work

--- server.py
+++ server.py
@@ -1,17 +1,51 @@
 import http.server
 import socketserver
+import sys
+import signal
 
-class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
+
+class Handler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         if self.path == '/':
             self.path = 'index.html'
-        return http.server.SimpleHTTPRequestHandler.do_GET(self)
+        return super().do_GET()
 
-# Create an object of the above class
-handler_object = MyHttpRequestHandler
+    def do_POST(self):
+        if self.path == '/save_settings':
 
-PORT = 8000
-server = socketserver.TCPServer(("", PORT), handler_object)
+            content_length = int(self.headers['Content-Length'])
+            post_data_bytes = self.rfile.read(content_length)
 
-# Star the server
-server.serve_forever()
+            post_data_str = post_data_bytes.decode("UTF-8")
+            post_data_str = post_data_str.replace("=", ":")
+            list_of_post_data = post_data_str.split('&')
+
+            print(list_of_post_data)
+
+        self.path = 'index.html'
+
+        return super().do_GET()
+
+# A custom signal handle to allow us to Ctrl-C out of the process
+
+
+if __name__ == "__main__":
+    def signal_handler(signal, frame):
+        print('Exiting http server (Ctrl+C pressed)')
+        try:
+            if (handler_object):
+                handler_object.server_close()
+        finally:
+            sys.exit(0)
+
+    signal.signal(signal.SIGINT, signal_handler)
+
+    # Create an object of the above class
+    handler_object = Handler
+
+    PORT = 8000
+
+    socketserver.TCPServer.allow_reuse_address = True
+    with socketserver.TCPServer(("", PORT), handler_object) as httpd:
+        print("serving at port", PORT)
+        httpd.serve_forever()
